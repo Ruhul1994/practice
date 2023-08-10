ecfa43c1b04da4e37211d9472ea9ed70a3e26bc9
Ruhul1994 <95877022+Ruhul1994@users.noreply.github.com>
2023-08-11 04:11:45 +0600

    Add files via upload
    Lesson 10 practice completed.

---
+++ lesson-10.js
@@ -0,0 +1,25 @@
+/*const buttontext = document.querySelector('button');
+console.log(buttontext.classList.contains('js-button'))*/
+
+/*
+function gamingclicking(){
+  const buttonGameElement = document.querySelector('.js-button-game ');
+  if( buttonGameElement.innerText === 'Gaming'){
+    buttonGameElement.classList.add('buttonGaming')
+    buttonGameElement.innerText= 'removeGame';
+  }else{
+    buttonGameElement.innerHTML= 'Gaming';
+    buttonGameElement.classList.remove('buttonGaming')
+  }
+}
+
+const inputElement = document.querySelector('input-number');
+function numberChecking(){
+  const inputValue = Number(inputElement.value);
+  if(inputValue <0){
+    document.querySelector(".js-paragraph-element ").innerHTML='Error: cost cannot be less than $0';
+  
+  }
+}*/
+const myArray = [10,12,13,34,65];
+console.log(typeof(myArray));
